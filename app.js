c808f046a46c82157ba37171371b2d8eef5f3516
Deepanshu <DeepanshuS@linxus.co.za>
2026-06-06 21:21:38 +0530

    Initial commit for Exercise 1.15

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("----------------------------------------");
+console.log("Hello MOOC! This is my Exercise 1.15 Homework app running inside Docker!");
+console.log("----------------------------------------");
